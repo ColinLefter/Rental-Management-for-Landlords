d6deb113e559a0446bb9f59e3c4f2fdd250072e7
Lily Yao <lilyyao102@gmail.com>
2023-12-04 21:06:20 -0800

    finished task1

--- app/src/main/java/com/example/rentalmanagementforlandlords/ui/majorTask1/task1.java
+++ app/src/main/java/com/example/rentalmanagementforlandlords/ui/majorTask1/task1.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.rentalmanagementforlandlords.R;
 import com.example.rentalmanagementforlandlords.databinding.Task1Binding;
 
 public class task1 extends Fragment {
@@ -18,14 +23,28 @@ public class task1 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-
-//        container.getParent()..setVisibility(View.GONE);
-
+        NavController navController = NavHostFragment.findNavController(this);
 
         task1ViewModel dashboardViewModel =
                 new ViewModelProvider(this).get(task1ViewModel.class);
 
         binding = Task1Binding.inflate(inflater, container, false);
+
+        View view = binding.getRoot();
+        Button addTenantButton = view.findViewById(R.id.task1_addtenant);
+        addTenantButton.setOnClickListener(l -> {
+            navController.navigate(R.id.action_majorTask1_to_task1_AddTenant);
+        });
+
+        Button viewConnectedAccountsButton = view.findViewById(R.id.task1_viewconnected);
+        viewConnectedAccountsButton.setOnClickListener(l -> {
+            navController.navigate(R.id.action_majorTask1_to_task1_ViewConnectedAccounts);
+        });
+
+        Button manageInvoicingButton = view.findViewById(R.id.task1_manageinvoicing);
+        manageInvoicingButton.setOnClickListener(l -> {
+            navController.navigate(R.id.action_majorTask1_to_task1_ManageInvoicing);
+        });
 
         return binding.getRoot();
     }
---
+++ app/src/main/java/com/example/rentalmanagementforlandlords/ui/majorTask1/task1_ManageInvoicing.java
@@ -0,0 +1,76 @@
+package com.example.rentalmanagementforlandlords.ui.majorTask1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.rentalmanagementforlandlords.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link task1_ManageInvoicing#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class task1_ManageInvoicing extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public task1_ManageInvoicing() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment task1_ManageInvoicing.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static task1_ManageInvoicing newInstance(String param1, String param2) {
+        task1_ManageInvoicing fragment = new task1_ManageInvoicing();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_task1__manage_invoicing, container, false);
+        NavController navController = NavHostFragment.findNavController(this);
+
+        Button addTenantButton = view.findViewById(R.id.task1_manage_submit);
+        addTenantButton.setOnClickListener(l -> {
+            navController.popBackStack();
+        });
+        return view;
+    }
+}
