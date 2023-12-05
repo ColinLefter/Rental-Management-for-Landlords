0dbb27e9515454fa97823040f9e24c243b172a39
Colin Lefter <colin.lefter02@gmail.com>
2023-12-04 19:06:19 -0800

    completed rough draft of major task 4

--- app/src/main/java/com/example/rentalmanagementforlandlords/ui/majorTask4/task4AccountsSummary.java
+++ app/src/main/java/com/example/rentalmanagementforlandlords/ui/majorTask4/task4AccountsSummary.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.rentalmanagementforlandlords.R;
 import com.example.rentalmanagementforlandlords.databinding.Task4AccountsSummaryBinding;
