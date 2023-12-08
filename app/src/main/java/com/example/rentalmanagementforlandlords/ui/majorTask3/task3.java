e9dfcb6354a726359cab7ab05f22a1edbc85fd59
LaWunn <kslawunn@gmail.com>
2023-12-07 18:18:50 -0800

    update task3

--- app/src/main/java/com/example/rentalmanagementforlandlords/ui/majorTask3/task3.java
+++ app/src/main/java/com/example/rentalmanagementforlandlords/ui/majorTask3/task3.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.rentalmanagementforlandlords.databinding.Task1Binding;
+import com.example.rentalmanagementforlandlords.R;
 import com.example.rentalmanagementforlandlords.databinding.Task3Binding;
 
 public class task3 extends Fragment {
@@ -24,7 +25,26 @@ public class task3 extends Fragment {
         binding = Task3Binding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        Button button = root.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToAnotherLayout();
+            }
+        });
+
         return root;
+    }
+
+    private void switchToAnotherLayout() {
+        // Inflate the layout from task_3_viewlistings.xml
+        View viewListingsView = getLayoutInflater().inflate(R.layout.task_3_viewlistings, null);
+
+        // Replace the current content with the new layout
+        ViewGroup rootView = (ViewGroup) getView();
+        rootView.removeAllViews();
+        rootView.addView(viewListingsView);
+        
     }
 
     @Override
---
+++ app/src/main/java/com/example/rentalmanagementforlandlords/ui/majorTask3/task3_Activity.java
@@ -0,0 +1,25 @@
+package com.example.rentalmanagementforlandlords.ui.majorTask3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rentalmanagementforlandlords.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class task3_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task3);
+
+
+    }
+}
